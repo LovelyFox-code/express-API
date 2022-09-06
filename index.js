910fb19f8959a365c1a3f1183771b1b38e7c1237
Alina <alinadakhno60@gmail.com>
2022-09-06 10:02:10 +0200

    change port

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
+const port = process.env.PORT || 5000;
 const app = express();
-const port = 5000;
 app.use(express.json());
 app.use(cors());
 let documents = [
